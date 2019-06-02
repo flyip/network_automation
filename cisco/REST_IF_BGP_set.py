1f885ab93b140f4f3b984bea8a2f6d370356f81c
flyip <flyipmm9@gmail.com>
2019-06-02 18:51:23 +0800

    1.Code beautify. 2.Add bgp loadbalace policy.

--- cisco/REST_IF_BGP_set.py
+++ cisco/REST_IF_BGP_set.py
@@ -4,7 +4,7 @@ import os
 from ncclient import manager
 import requests
 import json
-import jinja2 
+import jinja2
 import yaml
 import logging
 
@@ -17,7 +17,7 @@ myheaders={'content-type':'application/json-rpc'}
 
 
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s', filename='./logs/myapp.log')    
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s', filename='./logs/myapp.log')
 logger = logging.getLogger()
 # define a Handler which writes INFO messages or higher to the sys.stderr
 console = logging.StreamHandler()
@@ -27,7 +27,7 @@ formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
 # tell the handler to use this format
 console.setFormatter(formatter)
 # add the handler to the root logger
-logging.getLogger('').addHandler(console) 
+logging.getLogger('').addHandler(console)
 
 
 #create logs directory
@@ -35,7 +35,7 @@ print('Check logs directory!')
 if not os.path.exists(logs_dir):
     print('Creating logs directory')
     os.mkdir(logs_dir)
-    
+
 else:
     print('Logs directory is existing')
 
@@ -55,7 +55,7 @@ for key in data['device'].keys():
       #generate the configuration
       bgp_cfg = jinja2.Environment(loader=jinja2.FileSystemLoader('./templates')).get_template('bgp_nxos.j2').render(data=vars)
       interface_cfg = jinja2.Environment(loader=jinja2.FileSystemLoader('./templates')).get_template('interface_nxos.j2').render(data=vars)
-      commands = interface_cfg + bgp_cfg 
+      commands = interface_cfg + bgp_cfg
       list = [command.strip() for command in commands.split('\n') if command.strip() != '']
       payload = []
       i = 1
@@ -71,7 +71,7 @@ for key in data['device'].keys():
         "rollback": "rollback-on-error"
         }
         payload.append(template)
-        i += 1 
+        i += 1
       #Send REST request to NE.
       response = requests.post(url,data=json.dumps(payload), headers=myheaders,auth=(user,pw), verify=False).json()
 
@@ -96,7 +96,7 @@ for key in data['device'].keys():
                        device_params={'name': 'junos'},
                        hostkey_verify=False)
 
-      #Generate the configuration 
+      #Generate the configuration
       bgp_cfg = jinja2.Environment(loader=jinja2.FileSystemLoader('./templates')).get_template('bgp_junos.j2').render(data=vars)
       interface_cfg = jinja2.Environment(loader=jinja2.FileSystemLoader('./templates')).get_template('interface_junos.j2').render(data=vars)
       merge = bgp_cfg + '\n' + interface_cfg
@@ -109,4 +109,3 @@ for key in data['device'].keys():
       conn.unlock()
       conn.close_session()
 
-      
